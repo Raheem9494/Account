50d65d2fd23c7616185f6a70e9bff79ac0818f94
unknown <raheemmohammad43@gmail.com>
2024-10-22 15:16:24 +0530

    log changes

--- src/main/java/net/codejava/Controller/AccountController.java
+++ src/main/java/net/codejava/Controller/AccountController.java
@@ -1,5 +1,7 @@
 package net.codejava.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import net.codejava.Dto.AccountDto;
 import net.codejava.Service.AccountService;
-
+@Slf4j
 @RestController
 @RequestMapping("api/accounts")
 public class AccountController {
-
+ 
+	
 	//@Autowired
 	private AccountService accountService;
+	
 	
 	public AccountController(AccountService accountService) {
 		this.accountService = accountService;
@@ -24,7 +29,7 @@ public class AccountController {
 
 	@PostMapping
 	public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
-
+		log.info("AccountDto {}", accountDto);
 		return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
 	}
 
