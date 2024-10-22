e71ea23c259c70b5a429c8f43f6898361dfda67f
unknown <raheemmohammad43@gmail.com>
2024-10-22 15:58:13 +0530

    add DemoController

--- src/main/java/net/codejava/Controller/AccountController.java
+++ src/main/java/net/codejava/Controller/AccountController.java
@@ -1,15 +1,11 @@
 package net.codejava.Controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import net.codejava.Dto.AccountDto;
 import net.codejava.Service.AccountService;
